7b22a20f79d7d3506d84984b7f374e094c1ec388
ramees2415 <rameesck6937@gmail.com>
2026-01-28 07:18:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,535 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-exterior",
+      "name": "exterior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.04358734629646577,
+        "pitch": -0.07134789629620641,
+        "fov": 0.9852639548302736
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17303201577635896,
+          "pitch": 0.3648534275559392,
+          "rotation": 0,
+          "target": "1-recaption"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-recaption",
+      "name": "recaption",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6752739946250088,
+          "pitch": 0.23252909827251145,
+          "rotation": 6.283185307179586,
+          "target": "2-doctor-cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-doctor-cabin",
+      "name": "doctor cabin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6342334551056421,
+          "pitch": 0.3942849464766276,
+          "rotation": 0,
+          "target": "3-physiotherapy-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-physiotherapy-room",
+      "name": "physiotherapy room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3578909358802562,
+          "pitch": 0.1552567434589598,
+          "rotation": 0,
+          "target": "4-patient-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-patient-room",
+      "name": "patient room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4399831236515332,
+          "pitch": 0.4134889197106517,
+          "rotation": 0,
+          "target": "5-conference-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-conference-room",
+      "name": "conference room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3099348435135845,
+          "pitch": 1.2739787188497491,
+          "rotation": 3.141592653589793,
+          "target": "6-pharmacy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pharmacy",
+      "name": "pharmacy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3363940095106965,
+          "pitch": 1.3921537600590472,
+          "rotation": 7.853981633974483,
+          "target": "7-lab"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-lab",
+      "name": "lab",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8013410211971816,
+          "pitch": 1.1828987444211325,
+          "rotation": 7.853981633974483,
+          "target": "8-nurses-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-nurses-station",
+      "name": "Nurses Station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1929262789144763,
+          "pitch": 0.920644054437247,
+          "rotation": 0.7853981633974483,
+          "target": "9-normal-rooms"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-normal-rooms",
+      "name": "normal rooms",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5829253419517944,
+          "pitch": 0.9037967980530084,
+          "rotation": 0,
+          "target": "10-duplex-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-duplex-room",
+      "name": "duplex room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.015525149532196636,
+          "pitch": 0.23703941420490793,
+          "rotation": 6.283185307179586,
+          "target": "11-kitchen"
+        },
+        {
+          "yaw": 2.51949702308187,
+          "pitch": 1.3808498351818237,
+          "rotation": 0,
+          "target": "12-seating"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-seating",
+      "name": "seating",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19721281900546295,
+          "pitch": 0.04041647942450055,
+          "rotation": 10.995574287564278,
+          "target": "13-lift-loby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-lift-loby",
+      "name": "lift loby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "VALUE FIRST",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
